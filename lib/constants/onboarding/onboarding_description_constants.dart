1a8221c6038c7bb833e242ad1da24486ecd9449d
Emmanuel Ametepee <emmanuelamet11@gmail.com>
2022-01-21 11:59:14 -0800

    Code refactored for better readability and simplicity.

---
+++ lib/constants/onboarding/onboarding_description_constants.dart
@@ -0,0 +1,10 @@
+class OnboardingDescriptionConstant {
+  static const descriptionOne =
+      'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be';
+  static const descriptionTwo =
+      'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be';
+  static const descriptionThree =
+      'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be';
+  static const descriptionFour =
+      'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be';
+}
---
+++ lib/constants/onboarding/onboarding_title_constants.dart
@@ -0,0 +1,6 @@
+class OnboardingTitleConstant {
+  static const titleOne = 'BooksHub';
+  static const titleTwo = 'Verified Authors';
+  static const titleThree = 'Readers are Leaders';
+  static const titleFour = 'Books by Category';
+}
